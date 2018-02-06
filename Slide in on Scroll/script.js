4701a366c6cc9515e062cdcac3ade9d08ee1a41d
Marcus Goncalves <marcus.goncalves1@walmart.com>
2018-02-06 15:43:51 -0200

    Start Slide in on Scroll project

--- Slide in on Scroll/script.js
+++ Slide in on Scroll/script.js
@@ -20,3 +20,27 @@ function debounce(func, wait = 20, immediate = true) {
 }
 
 
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    console.log(slideInAt);
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    console.log(imageBottom);
+    const isHalsfShown = slideInAt > sliderImage.offsetTop;
+    console.log(isHalsfShown);
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    console.log(isNotScrolledPast);
+
+    if (isHalsfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
