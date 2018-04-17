1a34ae754f3b69fb11d95a69bf64b80f782bfee6
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2018-04-17 20:18:46 -0300

    Finished Local Storage Event Delegation

--- Local Storage Event Delegation/script.js
+++ Local Storage Event Delegation/script.js
@@ -1,20 +1,6 @@
 const addItems = document.querySelector('.add-items');
-const itemList = document.querySelector('.plates');
+const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
-
-function addItem(e) {
-  e.preventDefault();
-  const text = (this.querySelector('[name=item]')).value
-  const item = {
-    text,
-    done: false
-  };
-
-  items.push(item);
-  populateList(items, itemList);
-  localStorage.setItem('items', JSON.stringify(items));
-  this.reset();
-}
 
 function populateList(plates = [], plateList) {
   plateList.innerHTML = plates.map((plate, i) => {
@@ -27,10 +13,29 @@ function populateList(plates = [], plateList) {
   }).join('');
 }
 
+function addItem(e) {
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  };
+
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+  this.reset();
+}
+
 function toggleDone(e) {
-  console.log(e);
+  if (!e.target.matches('input')) return;
+  const el = e.target;
+  const index = (el.dataset.index);
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
 }
 
 addItems.addEventListener('submit', addItem);
-itemList.addEventListener('click', toggleDone);
-populateList(items, itemList);
+itemsList.addEventListener('click', toggleDone);
+populateList(items, itemsList);
