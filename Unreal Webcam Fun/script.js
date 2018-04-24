3281f481e1f8525171e472a7aac8e7dbb6b74b2c
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2018-04-24 01:21:06 -0300

    Finished Webcam Fun

--- Unreal Webcam Fun/script.js
+++ Unreal Webcam Fun/script.js
@@ -3,64 +3,33 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+const click = document.querySelector('.click');
 
 function getVideo() {
   navigator.mediaDevices.getUserMedia({video: true, audio: false})
-  .then(localMediaStream => {
-    video.src = window.URL.createObjectURL(localMediaStream);
-    video.play();
-  })
-  .catch(err => {
-    console.error(`Oh No`, err);
-  });
-}
-
-function paintToCanavas() {
-  const width = video.videoWidth;
-  const height = video.videoHeight;
-  canvas.width = width;
-  canvas.height = height;
-
-  return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
-    let pixels = ctx.getImageData(0, 0, width, height);
-    // mess with then
-    // pixels = redEffect(pixels);
-    pixels = rgbSplit(pixels);
-    ctx.globalAlpha = 0.8;
-    // put then back
-    ctx.putImageData(pixels, 0, 0);
-  }, 16);
-}
-
-function takePhoto() {
-  // play the sound
-  snap.currentTime = 0;
-  snap.play();
-
-  const data = canvas.toDataURL('image/jpeg');
-  const link = document.createElement('a');
-  link.href = data;
-  link.setAttribute('download', 'handsome');
-  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
-  strip.insertBefore(link, strip.firstChild);
+    .then(localMediaStream => {
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
 }
 
 function redEffect(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 100;
-    pixels.data[i + 1] = pixels.data[i + 1] - 50;
-    pixels.data[i + 2] = pixels.data[i + 2] + 0.5;
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
   }
   return pixels;
 }
 
 function rgbSplit(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i - 150] = pixels.data[i + 0];
-    pixels.data[i + 100] = pixels.data[i + 1];
-    pixels.data[i - 150] = pixels.data[i + 2];
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
   }
   return pixels;
 }
@@ -68,11 +37,12 @@ function rgbSplit(pixels) {
 function greenScreen(pixels) {
   const levels = {};
 
-  [...document.querySelectorAll('.rgb input')].forEach((input) => {
+  document.querySelectorAll('.rgb input').forEach((input) => {
     levels[input.name] = input.value;
+    console.log(levels);
   });
 
-  for (i = 0; i < pixels.data.length; i = i + 4) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
     red = pixels.data[i + 0];
     green = pixels.data[i + 1];
     blue = pixels.data[i + 2];
@@ -89,7 +59,48 @@ function greenScreen(pixels) {
     }
   }
 
+  return pixels;
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    // pixels = redEffect(pixels);
+
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+
+    pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 160);
+}
+
+function takePhoto(e) {
+  e.preventDefault();
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firsChild);
+}
 
 getVideo();
 
-video.addEventListener('canplay', paintToCanavas);
+video.addEventListener('canplay', paintToCanvas);
+click.addEventListener('click', takePhoto);
+
